c1d207d2ad745281ec5f57908c1bf53ed3c5172f
nolderosw <wesley150wow@gmail.com>
2017-06-10 11:46:43 -0300

    server+template

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
+var port = process.env.PORT || 5000;
+
+app.set('port', process.env.PORT || 5000);
+app.listen(port);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res){
+	res.render('HTML/index');
+});
+
+
+
